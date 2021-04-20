3dc3363088174dd837b100dff5c63f0ba8184306
Ícaro Vidal Freire <icarofreire7@gmail.com>
2021-04-20 16:11:50 -0300

    - script sobre dplyr - dadasets: anscombe

--- R/script_dplyr.R
+++ R/script_dplyr.R
@@ -43,4 +43,120 @@ dados_mpg %>%
       "auto(s5)"   = "automatica",
       "auto(s4)"   = "automatica"
     )
+  ) %>%
+  view()
+
+# Algo sobre a função `select()` ----------------------------------------------
+## Seleciona a variável `cilindros`
+dados_mpg %>%
+  select(cilindros)
+
+## Não seleciona a variável `cilindros`
+dados_mpg %>%
+  select(!cilindros)
+
+dados_mpg %>%
+  select(-cilindros)
+
+# Selecionando mais de uma variável
+dados_mpg %>%
+  select(fabricante, modelo, tracao)
+
+# Selecionando uma sequência de variáveis
+dados_mpg %>%
+  select(fabricante:tracao)
+
+dados_mpg %>%
+  select(1:7)
+
+# Misturar tudo
+dados_mpg %>%
+  select(fabricante, cilindrada:tracao, -ano, rodovia)
+
+# Algo sobre a função `filter()` ----------------------------------------------
+## Listando os elementos distintos
+dados_mpg %>%
+  distinct(fabricante)
+
+dados_mpg %>%
+  distinct(cilindrada) %>%
+  view()
+
+## Filtrando por operadores lógicos
+dados_mpg %>%
+  filter(fabricante == "toyota" & cilindrada > 3)
+
+## Filtrando pala posição das linhas
+dados_mpg %>%
+  slice(3:7, 100) %>%
+  view()
+
+# Algo sobre a função `summarase()` -------------------------------------------
+## Sumarizando de uma forma geral (?)
+dados_mpg %>%
+  summarise(
+    media_cidade   = mean(cidade),
+    mediana_cidade = median(cidade),
+    dp_cidade      = sd(cidade),
+    min_cidade     = min(cidade),
+    max_cidade     = max(cidade),
+    Q1             = quantile(cidade)[2]
   )
+
+## Sumarizando para os carros da Chevrolet
+### Filtrarndo e exibindo a média
+dados_mpg %>%
+  filter(fabricante == "chevrolet") %>%
+  summarise(media_cidade = mean(cidade))
+
+### Filtrando a fabricante e o combustível
+dados_mpg %>%
+  filter(fabricante == "chevrolet" & combustivel == "r") %>%
+  summarise(media_cidade = mean(cidade))
+
+## Simbiose entre `summarise()` e group_by()`
+dados_mpg %>%
+  group_by(fabricante) %>%
+  summarise(media_cidade = mean(cidade))
+
+## Tabela de Frequência
+### Usando `group_by`
+dados_mpg %>%
+  group_by(combustivel) %>%
+  summarise(
+    n = n(),            # frequência absoluta
+    f = (n / 234) * 100 # frequência relativa
+  )
+
+dados_mpg %>%
+  group_by(classe) %>%
+  summarise(
+    n = n()
+  )
+
+### Usando `count()`
+dados_mpg %>%
+  count(combustivel)
+
+dados_mpg %>%
+  count(classe)
+
+# Algo sobre a função `arrange()`----------------------------------------------
+## Arrumando de forma crescente
+dados_mpg %>%
+  group_by(fabricante) %>%
+  summarise(media_cidade = mean(cidade)) %>%
+  arrange(media_cidade)
+
+## Arrumando de forma decrescente
+dados_mpg %>%
+  group_by(fabricante) %>%
+  summarise(media_cidade = mean(cidade)) %>%
+  arrange(desc(media_cidade))
+
+## Arrumando de forma crescente e selecionando os 5 mais econômicos nesse grupo
+dados_mpg %>%
+  group_by(fabricante) %>%
+  summarise(media_cidade = mean(cidade)) %>%
+  arrange(desc(media_cidade)) %>%
+  top_n(5)
